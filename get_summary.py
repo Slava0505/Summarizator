a7c0d2febe6e679d3b360a4439df055f33e3a8cf
Slava0505 <31116179+Slava0505@users.noreply.github.com>
2022-05-26 13:44:40 +0700

    Exception were deleted

--- get_summary.py
+++ get_summary.py
@@ -29,25 +29,22 @@ def get_summary(input_file_path='data/example_text_ru.txt', out_dir = 'data/', m
     """
     if method == 'Luhn':
         summarizator = LuhnSummarizator()
-    try:
-        text = open(input_file_path, encoding='utf-8').read()
-    except:
-        log_something(f'При подгрузке файла что-то пошло не так')
+
+    text = open(input_file_path, encoding='utf-8').read()
     lang = lang_identify(text)
     print(f'Язык текста: {lang}')
     log_something(f'Язык текста определен успешно: {lang}')
 
-    try:
-        summed_text = summarizator.sum_text(text)
-    except:
-        log_something(f'При суммаризации что-то пошло не так')
 
+    summed_text = summarizator.sum_text(text)
     input_file_name = input_file_path.split('/')[-1]
     output_file_path = out_dir + 'summed_' + input_file_name
     out_file = open(output_file_path, 'w', encoding='utf-8')
     out_file.write(summed_text)
     out_file.close()
     log_something(f'Суммаризация звершена успешно. Создан файл {output_file_path}')
+
+
 
 if __name__ == '__main__':
     fire.Fire(get_summary)
